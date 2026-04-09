dd8441ee1346ae1bea75a1a8f7004d8209fbc66f
gotobed <3372114176@qq.com>
2026-04-09 23:05:40 +0800

    feat(api): 移除旧的文章相关的API模块，精简代码结构

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,5 @@
 // 导出所有API模块
 export * from './auth';
-export * from './article.ts';
 export * from './user.ts';
-export * from './post.ts';
 export * from './comment.ts';
 export * from './article';
